b2e9151a1d01133af9a07924d158848bf2bf0554
Thales Gabriel <thal3s49@hotmail.com>
2018-11-23 14:09:42 -0300

    Versão cheia de coisa

---
+++ AndroidStudioProjects/Traineeapp-master/app/src/main/java/com/example/thal3/myapplication/Curriculo.java
@@ -0,0 +1,13 @@
+package com.example.thal3.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Curriculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curriculo);
+    }
+}
